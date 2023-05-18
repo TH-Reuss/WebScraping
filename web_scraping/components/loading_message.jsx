4cd856a8d6a5846d68f96afc4319c1ddd08ebe04
Thomas Muñoz Reuss <thomasmuoz@gmail.com>
2023-05-18 04:01:09 -0400

    Agregue pantalla de carga y modifiqué el diseño movil (en proceso)

--- web_scraping/components/loading_message.jsx
+++ web_scraping/components/loading_message.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React from 'react';
+import { IconContext } from 'react-icons';
+import { FiLoader } from 'react-icons/fi';
 
-function loadingMessage(props){
-    return (
-        <div className='mb-[2.5vw]'>
-          <h4 className='font-Inter text-[1.2vw]'>{props.title}</h4>
-          <p className='font-Roboto font-extralight text-[1.2vw] mt-[0.2vw] w-[85%]'>{props.description}</p>
-          <div className='flex flex-row justify-between font-Roboto font-extralight text-[0.8vw] mt-[0.5vw] w-[60%]'>
-              <span>Ultima Actualización: {props.date}</span>
-              <span>Tienda: {props.store}</span>
-              <span>Precio: {props.price}</span>
-              <a href={props.product_url} target='_blank' className=' font-medium no-underline hover:underline'>Visitar Tienda</a>
-          </div>
+
+function LoadingMessage(props) {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-black flex flex-col justify-center items-center z-50">
+        <div className='m-auto flex flex-col pt-[20vh] items-center'>
+            <IconContext.Provider value={{ className: 'spinner-icon'}}>
+                <FiLoader  className='animate-spin mb-[5vh] text-[1vw] text-white'/>
+            </IconContext.Provider>
+            <p className=' text-white font-Roboto font-thin text-[1vw]'>{props.message}</p>
         </div>
-    );
+        <p className='mt-auto mb-[3vw] text-white font-Roboto font-thin text-[0.7vw]'>Si eres reclutador no olvides contratarme</p>
+        
+    </div>
+  );
 }
 
-export default loadingMessage
+export default LoadingMessage
--- web_scraping/components/product_card.jsx
+++ web_scraping/components/product_card.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function ProductCard(props) {
+
+    const [title, setTitle] = useState('')
+
+    useEffect(() => {
+      if(props.title.length > 70){
+        setTitle(props.title.slice(0,70) + ' . . .')
+      }
+    }, [])
+
     return (
-      <div className='mb-[2.5vw]'>
-        <h4 className='font-Inter text-[1.2vw]'>{props.title}</h4>
-        <p className='font-Roboto font-extralight text-[1.2vw] mt-[0.2vw] w-[85%]'>{props.description}</p>
-        <div className='flex flex-row justify-between font-Roboto font-extralight text-[0.8vw] mt-[0.5vw] w-[60%]'>
-            <span>Ultima Actualización: {props.date}</span>
-            <span>Tienda: {props.store}</span>
-            <span>Precio: {props.price}</span>
-            <a href={props.product_url} target='_blank' className=' font-medium no-underline hover:underline'>Visitar Tienda</a>
+      <div className='mb-[10vw] w-[100%]'>
+        <h4 className='font-Inter w-[100%] text-[1rem] md:text-[1.5rem] font-normal text-white text-justify'>{title}</h4>
+        <p className='font-Roboto font-extralight text-[1rem] md:text-[1.5rem] mt-[2vw] text-white text-justify'>{props.description}</p>
+        <div className='flex flex-row flex-wrap font-Roboto font-extralight text-[0.8rem] mt-[2vw] text-white'>
+            <span className='w-[100%]'>Ultima Actualización: {props.date}</span>
+            <span className='mr-[5%]'>Tienda: {props.store}</span>
+            <span className='mr-[5%]'>Precio: {props.price}</span>
+            <a href={props.product_url} target='_blank' className=' mr-[5%] font-medium no-underline hover:underline'>Visitar Tienda</a>
         </div>
       </div>
     );
