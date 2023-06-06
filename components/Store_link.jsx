1e2239edb5686e591f7852a317c2f365d4ef6054
Thomas Muñoz Reuss <thomasmuoz@gmail.com>
2023-06-06 13:17:54 -0400

    .

--- components/Store_link.jsx
+++ components/Store_link.jsx
@@ -1,4 +1,4 @@
-const Link = ({ href, target, customStyles, url }) => {
+const Store_link = ({ href, target, customStyles, url }) => {
   customStyles = customStyles || "";
   const className = `underline text-[0.8rem] font-light text-white ${customStyles}`;
   return (
@@ -8,4 +8,4 @@ const Link = ({ href, target, customStyles, url }) => {
   );
 };
 
-export default Link;
+export default Store_link;
